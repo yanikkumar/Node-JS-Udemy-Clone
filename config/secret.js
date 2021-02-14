34c6879cbadc06f5c84378ce57079658e8bfd3c7
Yanik Kumar <believemasters@gmail.com>
2021-02-14 18:19:53 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,13 +1,13 @@
 module.exports =   // it makes this file exportable so as to use database and port in other files
     {
         // database: 'mongodb+srv://root:123abc@codedemy-tvncz.mongodb.net/test?retryWrites=true&w=majority',
-        database: 'mongodb://root:123abc@codedemy-shard-00-00-tvncz.mongodb.net:27017,codedemy-shard-00-01-tvncz.mongodb.net:27017,codedemy-shard-00-02-tvncz.mongodb.net:27017/test?ssl=true&replicaSet=Codedemy-shard-0&authSource=admin&retryWrites=true&w=majority',
+        database: 'YOUR_DB',
         port: 8000,
-        secretKey: 'Yanik2103',
+        secretKey: 'SECRET',
 
         facebook: {
-            clientID: '2343953218996866',
-            clientSecret: 'b73236b00b5867c189bf221b857c646b',
+            clientID: 'YOUR_CLIENT_ID',
+            clientSecret: 'YOUR_CLIENT_SECRET',
             profileFields: ['emails', 'displayName'],
             callbackURL: 'http://localhost:8000/auth/facebook/callback',
             passReqToCallback: true,
